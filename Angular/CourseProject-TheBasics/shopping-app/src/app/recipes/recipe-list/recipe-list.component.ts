00c99aa3ba203d7bb619ae01f40f14c7b20e45c4
Jwaibel1 <joshuawaibel8@gmail.com>
2023-10-05 05:25:54 -0500

    updated shopping app

--- Angular/CourseProject-TheBasics/shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/CourseProject-TheBasics/shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,20 +8,25 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'Test Recipe',
       'This is a test',
-      'https://bakesbybrownsugar.com/wp-content/uploads/2022/01/Chocolate-Rugelach-4C.jpg'
+      'https://www.allrecipes.com/thmb/lkmpKIBFPRbfATPBoR8E57KimqM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/45921-crispy-and-creamy-doughnuts-DDMFS-4x3-4266734872014eeebaeddbe56452e18d.jpg'
     ),
     new Recipe(
-      'Test Recipe',
+      'Another Test Recipe',
       'This is a test',
-      'https://bakesbybrownsugar.com/wp-content/uploads/2022/01/Chocolate-Rugelach-4C.jpg'
+      'https://sugarspunrun.com/wp-content/uploads/2021/11/The-best-vanilla-cake-recipe-1-of-1.jpg'
     ),
   ];
 
   constructor() {}
 
   ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
